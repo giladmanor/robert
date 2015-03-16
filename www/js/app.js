a10c112697d1d25d146a7e4b607f84b0f7d43d76
Gilad <giladmanor01@gmail.com>
2015-03-16 15:57:15 +0200

    update

--- www/js/app.js
+++ www/js/app.js
@@ -1,94 +1,140 @@
-var success = function(){};
-var failure = function(e){
+var success = function() {
+};
+var failure = function(e) {
 	alert("Error " + e);
 };
 
-
-var btStop=function(){
-	bluetoothSerial.write(" ", success, failure);
-};
-var btForward=function(){
-	bluetoothSerial.write("a", success, failure);
-};
-var btBack=function(){
-	bluetoothSerial.write("b", success, failure);
-};
-var btRight=function(){
-	bluetoothSerial.write("c", success, failure);
-};
-var btLeft=function(){
-	bluetoothSerial.write("d", success, failure);
+var log = function(s) {
+	$(".log").html("<li>" + s + "</li>");
 };
 
+var btStop = function() {
+	log("-");
+	btWrite("-");
+};
+var lastCommand = "";
 
+var btWrite = function(c) {
+	if (lastCommand != c) {
+		lastCommand = c;
+		bluetoothSerial.write(c, success, failure);
+	}
+
+};
 
 var app = {
+	debug : false,
+	
+	setDebug : function() {
+		app.debug = true;
+		$(".device-list").hide();
+		$(".joystick").show();
+	},
 	init : function() {
 		document.addEventListener('deviceready', this.onDeviceReady, false);
-		$(".joysick-location").html(Math.round($(".joysick-dot").position().left)+" : "+Math.round($(".joysick-dot").position().top));
+		$(".joysick-location").html(Math.round($(".joysick-dot").position().left) + " : " + Math.round($(".joysick-dot").position().top));
 	},
 	onDeviceReady : function() {
+		if (localStorage.robert_mac) {
+			app.setDevice(localStorage.robert_mac, function() {
+				$(".device-list").hide();
+				$(".joystick").show();
+			}, function() {
+				app.listDevices();
+			});
+
+		}
+
+	},
+	listDevices : function() {
 		bluetoothSerial.isEnabled(function() {
-			bluetoothSerial.list(function(list){
-				list.forEach(function(item){
-					$(".device-list ul").append("<li onclick='app.setDevice(\""+item.address+"\")'>"+item.name+"</li>");
-					
+			var onSuccess = function() {
+				$(".device-list").hide();
+				$(".joystick").show();
+			};
+			var onFail = function() {
+				alert("Robert is not responding");
+			};
+			bluetoothSerial.list(function(list) {
+				list.forEach(function(item) {
+					if (item.address == localStorage.robert_mac) {
+						$(".device-list ul").append("<li onclick='app.setDevice(\"" + item.address + "\"," + onSuccess + "," + onFail + ")'><img src='img/icon.png' style='max-height: 1em' /></li>");
+					} else {
+						$(".device-list ul").append("<li onclick='app.setDevice(\"" + item.address + "\"," + onSuccess + "," + onFail + ")'>" + item.name + "</li>");
+					}
+
 				});
-			}, function(){
+
+			}, function() {
 				alert("could not get the list of devices, boo hoo...");
 			});
 		}, function() {
-			alert("Please turn on Bluetooth");
+			$(".device-list ul").append("<li onclick='app.setDevice()'>debug</li>");
 		});
+	},
+	joysickStart : function(event) {
+		//alert("start");
+		app.joysick = {
+			x : event.touches[0].pageX,
+			y : event.touches[0].pageY
+		};
 
 	},
-	joysickStart:function(event){
-		//alert("start");
-		app.joysick = {x:event.touches[0].pageX,y:event.touches[0].pageY};
-		
-	},
-	
-	joysickMove:function(event){
+
+	joysickMove : function(event) {
 		var x = event.touches[0].pageX - app.joysick.x;
 		var y = event.touches[0].pageY - app.joysick.y;
-		
-		$(".joysick-location").html(Math.round(x)+" : "+Math.round(y));
-		$(".joysick-dot").css("transform","translate("+x+"px, "+y+"px)");
+
+		$(".joysick-location").html(Math.round(x) + " : " + Math.round(y));
+		$(".joysick-dot").css("transform", "translate(" + x + "px, " + y + "px)");
+		//log(x + ":" + y);
+		app.sendBT(x, y);
 	},
-	
-	joysickEnd:function(event){
-		$(".joysick-dot").css("transform","translate(0,0)");
-		$(".joysick-location").html(Math.round($(".joysick-dot").position().left)+" : "+Math.round($(".joysick-dot").position().top));
-		
+
+	joysickEnd : function(event) {
+		$(".joysick-dot").css("transform", "translate(0,0)");
+		$(".joysick-location").html(Math.round($(".joysick-dot").position().left) + " : " + Math.round($(".joysick-dot").position().top));
+
 		btStop();
-		
+
 	},
-	setDevice:function(macAddress){
-		bluetoothSerial.connectInsecure(macAddress, function(d){
-			alert("success " + d);
-			$(".device-list").hide();
-			$(".joystick").show();
-		}, function(e){
-			alert("Error " + e);
-		});
-	},
-	
-	sendBT:function(x,y){
-		if(x<0){
-			btForward();
-		}else if(x>0){
-			btBack();
+	setDevice : function(macAddress, onSuccess, onFail) {
+		$(".face").show();
+		if (macAddress) {
+			localStorage.robert_mac = macAddress;
+			bluetoothSerial.connectInsecure(macAddress, function(d) {
+				onSuccess();
+				$(".face").hide();
+			}, function(e) {
+				$(".face").hide();
+				onFail();
+			});
 		}
-		
-		if(y>0){
-			btLeft();
-		}else if(y<0){
-			btRight();
-		}
-		
-		
-		
+
 	},
-	
-	
+
+	sendBT : function(x, y) {
+		if (Math.abs(x) < 10 && Math.abs(y) < 10) {
+			btStop();
+			return;
+		}
+		if (Math.abs(x) > Math.abs(y)) {
+			if (x < 0) {
+				log("left");
+				btWrite("c");
+			} else if (x > 0) {
+				log("right");
+				btWrite("d");
+			}
+		} else {
+			if (y > 0) {
+				log("back");
+				btWrite("b");
+			} else if (y < 0) {
+				log("forward");
+				btWrite("a");
+			}
+		}
+
+	},
 };
